7e3a97340201325c5ca8abd68fba556b3ff084fa
gracece10 <gresce10@gmail.com>
2023-09-28 20:06:07 +0800

    first commit

---
+++ bin/belajar.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  String nama = "rizqi";
+  num umur = 20;
+  bool isBelumMenikah = true;
+
+  print("nama saya $nama");
+  print("umur saya $umur");
+  print("saya belum menikah $isBelumMenikah");
+}
